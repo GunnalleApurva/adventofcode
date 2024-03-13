59424327e261eb4519b4b41e56ad75b79683618a
GunnalleApurva <77142378+GunnalleApurva@users.noreply.github.com>
2024-03-13 15:50:52 +0530

    Update Day3GearRatios.cpp
    still doesn't work

--- Day3GearRatios.cpp
+++ Day3GearRatios.cpp
@@ -1,12 +1,9 @@
-//https://adventofcode.com/2023/day/3
 #include <bits/stdc++.h>
-#include <cctype>
 using namespace std;
 
 bool isSymbol(char ch)
 {
     return (!isdigit(ch) && ch!='.') ;
-    
 }
 
 bool isAdjToSymbol(vector<string> &v, int i, int j)
@@ -24,7 +21,7 @@ bool isAdjToSymbol(vector<string> &v, int i, int j)
     {
         if (isSymbol(v[i + 1][j]))
             return true;
-        if (j > 0 && (isSymbol(v[i + 1][j - 1] || v[i][j - 1])))
+        if (j > 0 && (isSymbol(v[i + 1][j - 1] || isSymbol(v[i][j - 1]))))
             return true;
         if (j < v[i].size() - 1 && (isSymbol(v[i][j + 1]) || isSymbol(v[i + 1][j + 1])))
             return true;
@@ -38,8 +35,7 @@ int main()
     vector<string> s;
     while (getline(cin, str))
     {
-        if (str.empty())
-        {
+        if (str.empty()) {
             break;
         }
         s.push_back(str);
@@ -51,16 +47,14 @@ int main()
         if (valid)
         {
             ans += num; 
-            num = 0;
-            valid = false;
         }
+        num = 0;
+        valid = false;
         for (int j = 0; j < s[i].size(); j++)
         {
             if (isdigit(s[i][j]))
             {
-                if(!num|| (j!=s[i].size()-1 && !isdigit(s[i][j+1])) || (j==s[i].size()-1)  ){
                 valid = valid || isAdjToSymbol(s, i, j);
-                } // checking only for first and last digits of the number 
                 num = num * 10 + (s[i][j] - '0');
             }
             else
@@ -77,8 +71,6 @@ int main()
     if (valid)
     {
         ans += num;
-        num = 0;
-        valid = false;
     }
     cout << ans;
 }
